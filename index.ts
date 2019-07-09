e890151fff56ae1399bf0fabc171efaf653811b7
Moniv9 <mohitverma3222@gmail.com>
2019-07-09 20:23:40 +0530

    adding progress bar class with various methods

--- index.ts
+++ index.ts
@@ -1,7 +1,40 @@
+export class ProgressBar {
 
-import './style.css';
+  private selector: string;
+  private progressBar: HTMLElement;
+  private intervalInstance: any;
+  private currentPercentage: number;
 
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+  constructor(private percentage: number) {
+    this.selector = 'progress-bar';
+    this.progressBar = document.getElementById(this.selector);
+    this.currentPercentage = 0;
+  }
 
-const 
+  setStyle(color: string): void {
+    this.progressBar.style.width = '0%';
+    this.progressBar.style.height = '20px';
+    this.progressBar.style.backgroundColor = color;
+  }
+
+  showProgress(): void {
+    this.currentPercentage += 10;
+    this.progressBar.style.width = `${this.currentPercentage}%`;
+
+    if (this.currentPercentage >= this.percentage) {
+      clearInterval(this.intervalInstance);
+    }
+  }
+
+  render(color: string = 'orange'): void {
+    this.setStyle(color);
+    this.intervalInstance = setInterval(this.showProgress.bind(this), 80);
+  }
+}
+
+const progressBar = new ProgressBar(100);
+progressBar.render();
+
+
+
+
