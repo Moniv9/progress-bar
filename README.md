4d1e7058cc53802c437cd98476e3730d7f5f378e
Moniv9 <mohitverma3222@gmail.com>
2019-07-09 11:53:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-wzitj5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-wzitj5)
---
+++ index.ts
@@ -0,0 +1,7 @@
+
+import './style.css';
+
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const 
